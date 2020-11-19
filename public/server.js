b7d31acfb8e7b27fbbad4d0817d3021241b57824
BetterBusinessBrian <bman1402@gmail.com>
2020-11-19 14:14:34 -0500

    full functionality

--- public/server.js
+++ public/server.js
@@ -4,43 +4,56 @@ var path = require("path");
 
 var app = express();
 var PORT = 6060;
+let tempID = 1
 
+app.use(express.static('../public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), (err) =>{if(err) throw err}));
+let notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), (err) => { if (err) throw err }));
 
 // HTML Routes
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-  });
-  
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
-// API Routes
-  app.get("/api/notes", function(req, res) {
-    return res.json(notes);
-  });
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
 
-  app.post("/api/notes", function(req, res) {
-    var newNote = req.body;
-    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
-    notes.push(newNote);
-    res.json(notes)
-  });
-  
-  
-  // Displays all reservations
-app.delete("/api/notes/:id", function(req, res) {
-    fs.readFile
-  });
-  
-  
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-    console.log(notes)
-  });
-  
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+// API Routes
+app.get("/api/notes", function (req, res) {
+  return (res.json(notes));
+});
+
+app.post("/api/notes", function (req, res) {
+  var newNote = req.body;
+  req.body.id = tempID;
+  tempID++;
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => { if (err) throw err });
+  res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+
+// Delete Note
+app.delete("/api/notes/:id", function (req, res) {
+
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id == req.params.id) {
+      notes.splice(i, 1);
+    } 
+
+
+  }
+  fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => { if (err) throw err });
+  res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+  console.log(notes)
+  console.log(tempID)
+});
