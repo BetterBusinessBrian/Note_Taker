e4925636fb52380e3c0e6ee32ab6da5263b487aa
BetterBusinessBrian <bman1402@gmail.com>
2020-11-19 14:47:22 -0500

    adjusted express.static line for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var PORT = process.env.PORT || 3001;
 let tempID = 1
 
-app.use(express.static(path.join('/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
